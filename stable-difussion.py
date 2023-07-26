e90a0bad427e3f5685d6fcf3973c9fb8fa29ca13
Angel Garcia <angelgarciaweb@gmail.com>
2023-07-26 11:20:47 -0400

    ✨ feat: add first practice with stable diffusion

---
+++ stable-difussion.py
@@ -0,0 +1,24 @@
+from diffusers import DiffusionPipeline
+import torch
+import cv2
+
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-0.9", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
+
+# if using torch < 2.0
+# pipe.enable_xformers_memory_efficient_attention()
+
+prompt = "An astronaut riding a green horse"
+
+image = pipe(prompt=prompt, output_type="latent").images
+
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
+
+# if using torch < 2.0
+# pipe.enable_xformers_memory_efficient_attention()
+
+images = pipe(prompt=prompt, image=image).images
+
+
+cv2.imshow(images)
